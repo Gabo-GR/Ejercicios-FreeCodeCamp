3a25deafb46fe2283e095cc48bf16af64ed98f2f
Gabo-GR <ggonzalezr1400@alumno.ipn.mx>
2024-11-05 21:39:52 -0600

    Se corrige error de sintaxis que no mostraba el formulario de evento

--- Javascript Algorithms and Data Structures/Todo App/script.js
+++ Javascript Algorithms and Data Structures/Todo App/script.js
@@ -1,47 +1,50 @@
-const taskForm = document.getElementById('task-form');
-const confirmCloseDialog = document.getElementById('confirm-close-dialog');
-const openTaskFormBtn = document.getElementById('open-task-form-btn');
-const closeTaskFormBtn = document.getElementById('close-task-form-btn');
-const addOrUpdateTaskBtn = document.getElementById('add-or-update-task-btn');
-const cancelBtn = document.getElementById('cancel-btn');
-const discardBtn = document.getElementById('discard-btn');
-const tasksContainer = document.getElementById('tasks-container');
-const titleInput = document.getElementById('title-input');
-const dateInput = document.getElementById('date-input');
-const descriptionInput = document.getElementById('description-input');
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
+const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const discardBtn = document.getElementById("discard-btn");
+const tasksContainer = document.getElementById("tasks-container");
+const titleInput = document.getElementById("title-input");
+const dateInput = document.getElementById("date-input");
+const descriptionInput = document.getElementById("description-input");
 
 const taskData = [];
 let currentTask = {};
 
 const addOrUpdateTask = () => {
-    const dataArrIndex = taskData.findIndex(item => item.id === currentTask.id);
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
     const taskObj = {
         id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
         title: titleInput.value,
         date: dateInput.value,
         description: descriptionInput.value,
-    }
+    };
     console.log(taskObj);
     if (dataArrIndex === -1) {
         taskData.unshift(taskObj);
     }
-    updateTaskContainer();
-    reset();
+    updateTaskContainer()
+    reset()
 };
 
 const updateTaskContainer = () => {
     tasksContainer.innerHTML = "";
-    taskData.forEach(({ id, title, date, description }) => {
-        tasksContainer.innerHTML += `
-            <div class="task" id="${id}">
+
+    taskData.forEach(
+        ({ id, title, date, description }) => {
+            tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
             <p><strong>Title:</strong>${title}</p>
             <p><strong>Date:</strong>${date}</p>
             <p><strong>Description:</strong>${description}</p>
             <button type="button" class="btn">Edit</button>
             <button type="button" class="btn">Delete</button>
-            </div>
+        </div>
         `
-    });
+        }
+    );
 };
 
 const reset = () => {
@@ -50,44 +53,31 @@ const reset = () => {
     descriptionInput.value = "";
     taskForm.classList.toggle("hidden");
     currentTask = {};
-};
+}
 
 openTaskFormBtn.addEventListener("click", () =>
-    taskForm.classList.toogle("hidden")
+    taskForm.classList.toggle("hidden")
 );
 
 closeTaskFormBtn.addEventListener("click", () => {
-    
     const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
     if (formInputsContainValues) {
         //showModal es un metodo que muestra el dialogo modal en la pantalla del usuario
         confirmCloseDialog.showModal();
     } else {
-        reset();
+        reset()
     }
 });
 
-cancelBtn.addEventListener("click", () =>
-    confirmCloseDialog.close()
-);
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
 
 discardBtn.addEventListener("click", () => {
     confirmCloseDialog.close();
-    reset();
+    reset()
 });
 
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const dataArrIndex = taskData.findIndex(item => item.id === currentTask.id);
-    const taskObj = {
-        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-        title: titleInput.value,
-        date: dateInput.value,
-        description: descriptionInput.value,
-    }
-    console.log(taskObj);
-    if (dataArrIndex === -1) {
-        taskData.unshift(taskObj);
-    }
+
     addOrUpdateTask();
 });
