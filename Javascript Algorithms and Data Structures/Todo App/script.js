ab006c48ea07c580aa62b2636537e8057eed87cc
Gabo-GR <ggonzalezr1400@alumno.ipn.mx>
2024-11-12 14:01:59 -0600

    Todo App terminada  y funcional

--- Javascript Algorithms and Data Structures/Todo App/script.js
+++ Javascript Algorithms and Data Structures/Todo App/script.js
@@ -10,21 +10,36 @@ const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
-const taskData = [];
+// JSON.parse convierte un string en un objeto
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
 
+const removeSpecialChars = (val) => {
+    return val.trim().replace(/[^A-Za-z0-9\-\s]/g, '')
+  }  
+
 const addOrUpdateTask = () => {
+    if (!titleInput.value.trim()) {
+        alert("Please provide a title");
+        return;
+    }  
+    //Se busca el indice del objeto que se esta editando en el arreglo de objetos taskData para poder actualizarlo en el arreglo de objetos taskData
     const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
     const taskObj = {
-        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-        title: titleInput.value,
+        //split divide un string en un array de strings, join une los elementos de un array en un string
+        id: `${removeSpecialChars(titleInput.value).toLowerCase().split(" ").join("-")}-${Date.now()}`,
+        title:removeSpecialChars(titleInput.value) ,
         date: dateInput.value,
-        description: descriptionInput.value,
+        description: removeSpecialChars(descriptionInput.value),
     };
     console.log(taskObj);
     if (dataArrIndex === -1) {
         taskData.unshift(taskObj);
     }
+    else {
+        taskData[dataArrIndex] = taskObj;
+    }
+    localStorage.setItem("data", JSON.stringify(taskData));
     updateTaskContainer()
     reset()
 };
@@ -39,20 +54,42 @@ const updateTaskContainer = () => {
             <p><strong>Title:</strong>${title}</p>
             <p><strong>Date:</strong>${date}</p>
             <p><strong>Description:</strong>${description}</p>
-            <button type="button" class="btn">Edit</button>
-            <button type="button" class="btn">Delete</button>
+            <button type="button" class="btn" onclick="editTask(this)" >Edit</button>
+            <button type="button" class="btn" onclick="deleteTask(this)">Delete</button>
         </div>
         `
         }
     );
 };
 
+const deleteTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
+    buttonEl.parentElement.remove();
+    taskData.splice(dataArrIndex, 1);
+    localStorage.setItem("data", JSON.stringify(taskData));
+};
+
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
+    currentTask = taskData[dataArrIndex];
+    titleInput.value = currentTask.title;
+    dateInput.value = currentTask.date;
+    descriptionInput.value = currentTask.description;
+    addOrUpdateTaskBtn.innerText = "Update Task";
+    taskForm.classList.toggle("hidden");
+};
+
 const reset = () => {
+    addOrUpdateTaskBtn.innerText = "Add Task";
     titleInput.value = "";
     dateInput.value = "";
     descriptionInput.value = "";
     taskForm.classList.toggle("hidden");
     currentTask = {};
+}
+
+if(taskData.length) {
+    updateTaskContainer()
 }
 
 openTaskFormBtn.addEventListener("click", () =>
@@ -61,7 +98,8 @@ openTaskFormBtn.addEventListener("click", () =>
 
 closeTaskFormBtn.addEventListener("click", () => {
     const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
-    if (formInputsContainValues) {
+    const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
+    if (formInputsContainValues && formInputValuesUpdated) {
         //showModal es un metodo que muestra el dialogo modal en la pantalla del usuario
         confirmCloseDialog.showModal();
     } else {
