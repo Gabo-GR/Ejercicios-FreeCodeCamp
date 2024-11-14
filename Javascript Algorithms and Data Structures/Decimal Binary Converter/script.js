048ca976f8cf0c8e06c21fbb598147a7bdb762f7
Gabo-GR <ggonzalezr1400@alumno.ipn.mx>
2024-11-13 20:55:23 -0600

    Se avanza al paso 22

--- Javascript Algorithms and Data Structures/Decimal Binary Converter/script.js
+++ Javascript Algorithms and Data Structures/Decimal Binary Converter/script.js
@@ -2,8 +2,29 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const decimalToBinary = (input) => {
+  let inputs = [];
+  let quotients = [];
+  let remainders = [];
+  input = 0;
+}
+
 const checkUserInput = () => {
-    console.log(numberInput.value);
+    if(
+      !numberInput.value ||
+      isNaN(numberInput.value) ||
+      parseInt(numberInput.value) < 0
+    ){
+      alert("Please provide a decimal number greater than or equal to 0");
+      return;
+    }
+    decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = "";
   };
 
 convertBtn.addEventListener("click", checkUserInput);
+numberInput.addEventListener("keydown", (e) => {
+  if(e.key === "Enter") {
+    checkUserInput();
+  }
+});
